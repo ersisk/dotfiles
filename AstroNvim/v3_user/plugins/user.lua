9a7e5da3f96cb0d46adb491e930e1f591d32b700
Erşan Işık <ersanisik@teknasyon.com>
2023-03-16 13:47:18 +0300

    [feat] add personal nvim config for v3

---
+++ AstroNvim/v3_user/plugins/user.lua
@@ -0,0 +1,16 @@
+return {
+  -- You can also add new plugins here as well:
+  -- Add plugins, the lazy syntax
+  -- "andweeb/presence.nvim",
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   event = "BufRead",
+  --   config = function()
+  --     require("lsp_signature").setup()
+  --   end,
+  -- },
+   { "jwalton512/vim-blade" },
+   { "ekalinin/Dockerfile.vim" },
+   { "mg979/vim-visual-multi" },
+   { "mralejandro/vim-phpdoc"}
+}
