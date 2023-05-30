4e5992b9fd9dc5ae3ae7614a72be7162ef58fbf0
Erşan Işık <ersanisik@teknasyon.com>
2023-05-30 13:55:19 +0300

    [chore] update colorschemes

--- AstroNvim/v3_user/plugins/community.lua
+++ AstroNvim/v3_user/plugins/community.lua
@@ -4,6 +4,6 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.colorscheme.github-nvim-theme" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
