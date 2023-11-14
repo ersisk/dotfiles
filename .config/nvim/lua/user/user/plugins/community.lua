681460b58f6db5930e11554b49785b00c3ece85c
Erşan Işık <ersanisik@teknasyon.com>
2023-11-14 19:12:09 +0300

    [fix] update nvim status bar

--- .config/nvim/lua/user/user/plugins/community.lua
+++ .config/nvim/lua/user/user/plugins/community.lua
@@ -4,7 +4,6 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.bars-and-lines.bufferline-nvim" },
-  { import = "astrocommunity.bars-and-lines.lualine-nvim" },
   { import = "astrocommunity.colorscheme.dracula-nvim" },
   { import = "astrocommunity.note-taking.obsidian-nvim" },
   { import = "astrocommunity.programming-language-support.rest-nvim" },
