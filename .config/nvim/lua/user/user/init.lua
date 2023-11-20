a6dfbc4fb35a29c4ea419e422efdd439848e7163
Erşan Işık <ersanisik@teknasyon.com>
2023-11-20 10:03:34 +0300

    [chore] theme change

--- .config/nvim/lua/user/user/init.lua
+++ .config/nvim/lua/user/user/init.lua
@@ -18,7 +18,7 @@ return {
   },
   -- Set colorscheme to use
   -- colorscheme = "astrodark",
-  colorscheme = "dracula",
+  colorscheme = "nordic",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = true,
