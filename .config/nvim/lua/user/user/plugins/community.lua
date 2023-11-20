0fe8b8415b5ac1601e7c28553f409168c13d6cd4
Erşan Işık <ersanisik@teknasyon.com>
2023-11-20 14:17:24 +0300

    [chore] update starship

--- .config/nvim/lua/user/user/plugins/community.lua
+++ .config/nvim/lua/user/user/plugins/community.lua
@@ -4,6 +4,7 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.bars-and-lines.heirline-vscode-winbar" },
+  { import = "astrocommunity.bars-and-lines.heirline-mode-text-statusline" },
   { import = "astrocommunity.colorscheme.nordic-nvim" },
   { import = "astrocommunity.note-taking.obsidian-nvim" },
   { import = "astrocommunity.programming-language-support.rest-nvim" },
@@ -12,6 +13,6 @@ return {
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   { import = "astrocommunity.motion.vim-matchup" },
-  { import = "astrocommunity.motion.mini-move", enabled = true },
+  { import = "astrocommunity.lsp.lsp-signature-nvim", enabled = true },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
