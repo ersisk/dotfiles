d409445a2de8204ab1190d1b74d72d2c9cae6241
Erşan Işık <ersanisik@teknasyon.com>
2024-02-12 13:16:41 +0300

    [feat] add nvim not distro config

---
+++ .config/nvim-not-distro/lua/plugins/debugging.lua
@@ -0,0 +1,11 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"leoluz/nvim-dap-go",
+		"rcarriga/nvim-dap-ui",
+	},
+	config = function()
+		require("dapui").setup()
+		require("dap-go").setup()
+	end,
+}
