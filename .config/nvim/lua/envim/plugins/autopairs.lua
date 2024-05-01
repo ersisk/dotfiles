95fe6482e63f8dc574369cd18d6e8d6372dc8625
Erşan Işık <ersanisik@teknasyon.com>
2024-05-01 19:30:14 +0300

    [feat] vscode keys

--- .config/nvim/lua/envim/plugins/autopairs.lua
+++ .config/nvim/lua/envim/plugins/autopairs.lua
@@ -10,13 +10,25 @@ return {
 
     -- configure autopairs
     autopairs.setup({
-      check_ts = true, -- enable treesitter
+      check_ts = true,                      -- enable treesitter
       ts_config = {
-        lua = { "string" }, -- don't add pairs in lua string treesitter nodes
+        lua = { "string" },                 -- don't add pairs in lua string treesitter nodes
         javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
-        java = false, -- don't check treesitter on java,
+        java = false,                       -- don't check treesitter on java,
         php = false
       },
+      fast_wrap = {
+        map = "<M-e>",
+        chars = { "{", "[", "(", '"', "'" },
+        pattern = ([[ [%'%"%)%>%]%)%}%,] ]]):gsub("%s+", ""),
+        offset = 0,
+        end_key = "$",
+        keys = "qwertyuiopzxcvbnmasdfghjkl",
+        check_comma = true,
+        highlight = "PmenuSel",
+        highlight_grey = "LineNr",
+      },
+      ""
     })
 
     -- import nvim-autopairs completion functionality
--- .config/nvim/lua/envim/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/envim/plugins/lsp/lspconfig.lua
@@ -27,13 +27,15 @@ return {
 
         -- set keybinds
         opts.desc = "Show LSP references"
+        vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format, {})
         keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+        keymap.set("n", "gr", vim.lsp.buf.references, opts) -- show definition, references
 
         opts.desc = "Go to declaration"
-        keymap.set("n", "gD", vim.lsp.buf.declaration, opts) -- go to declaration
+        keymap.set("n", "gd", vim.lsp.buf.declaration, opts) -- go to declaration
 
         opts.desc = "Show LSP definitions"
-        keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+        keymap.set("n", "gD", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
 
         opts.desc = "Show LSP implementations"
         keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
@@ -42,10 +44,10 @@ return {
         keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
         opts.desc = "See available code actions"
-        keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+        keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
         opts.desc = "Smart rename"
-        keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+        keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts) -- smart rename
 
         opts.desc = "Show buffer diagnostics"
         keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
