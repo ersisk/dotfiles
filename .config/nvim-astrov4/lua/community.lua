1a868eead12ce5a0a793e9e52a2a721151205d23
Erşan Işık <ersanisik@teknasyon.com>
2024-11-25 17:41:41 +0300

    [feat] add copilot

--- .config/nvim-astrov4/lua/community.lua
+++ .config/nvim-astrov4/lua/community.lua
@@ -6,17 +6,17 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
-  { import = "astrocommunity.color.transparent-nvim" },
-  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
-  --{ import = "astrocommunity.programming-language-support.rest-nvim" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
   { import = "astrocommunity.pack.blade" },
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  { import = "astrocommunity.color.transparent-nvim" },
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  --{ import = "astrocommunity.programming-language-support.rest-nvim" },
   { import = "astrocommunity.markdown-and-latex.glow-nvim" },
   { import = "astrocommunity.motion.vim-matchup" },
   { import = "astrocommunity.motion.harpoon" },
@@ -24,43 +24,26 @@ return {
   { import = "astrocommunity.lsp.nvim-lint", enabled = true },
   { import = "astrocommunity.file-explorer.oil-nvim" },
   -- { import = "astrocommunity.completion.codeium-nvim" },
-  { import = "astrocommunity.completion.codeium-vim" },
+  -- { import = "astrocommunity.completion.codeium-vim" },
+  { import = "astrocommunity.completion.copilot-cmp" },
+  { import = "astrocommunity.editing-support.copilotchat-nvim" },
   { import = "astrocommunity.note-taking.obsidian-nvim" },
   { import = "astrocommunity.recipes.telescope-lsp-mappings" },
+  { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
   -- import/override with your plugins folder
-  {
-    "oil.nvim",
-    lazy = false,
-    opts = {
-      skip_confirm_for_simple_edits = true,
-      delete_to_trash = false,
-      trash_command = "trash",
-      -- view_options = {
-      --   -- Show files and directories that start with "."
-      --   show_hidden = true,
-      -- },
-    },
-    keys = {
-      {
-        "-",
-        function() return require("oil").open_float() end,
-        desc = "Open parent directory",
-      },
-    },
-  },
   {
     "m4xshen/smartcolumn.nvim",
     opts = {
       colorcolumn = "118",
     },
   },
-  {
-    "Exafunction/codeium.vim",
-    enabled = true,
-    version = "1.8.37",
-    event = "InsertEnter",
-    config = function()
-      vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true, silent = true })
-    end,
-  },
+  -- {
+  --   "Exafunction/codeium.vim",
+  --   enabled = true,
+  --   version = "1.8.37",
+  --   event = "InsertEnter",
+  --   config = function()
+  --     vim.keymap.set("i", "<C-g>", function() return vim.fn["codeium#Accept"]() end, { expr = true, silent = true })
+  --   end,
+  -- },
 }
