325ebfca74b0485ea82d5e78939e977c5feadb0a
Erşan Işık <ersanisik@teknasyon.com>
2025-04-25 17:41:44 +0300

    [fix(envim)] gotofile command

--- .config/envim/lua/utils/commands.lua
+++ .config/envim/lua/utils/commands.lua
@@ -16,7 +16,8 @@
 --
 -- snacks picker
 vim.cmd([[command! -nargs=0 GoToCommand :lua Snacks.picker.command_history()]])
-vim.cmd([[command! -nargs=0 GoToFile :lua Snacks.picker.smart()]])
+vim.cmd([[command! -nargs=0 GoToFile :lua Snacks.picker.files({ hidden = true, ignored = true })]])
 vim.cmd([[command! -nargs=0 GoToSymbol :lua Snacks.picker.lsp_symbols()]])
 vim.cmd([[command! -nargs=0 GoToBuffer :lua Snacks.picker.buffers()]])
 vim.cmd([[command! -nargs=0 Grep :lua Snacks.picker.grep()]])
+vim.cmd([[command! -nargs=0 SmartGoTo :lua Snacks.picker.smart()]])
