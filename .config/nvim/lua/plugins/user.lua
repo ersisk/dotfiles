44a07eac634d04064821bd0e14705985d5b08058
Erşan Işık <ersanisik@teknasyon.com>
2025-09-15 15:22:49 +0300

    feat(nvim) add newfile plugin

--- .config/nvim/lua/plugins/user.lua
+++ .config/nvim/lua/plugins/user.lua
@@ -45,4 +45,15 @@ return {
     ---@type render.md.UserConfig
     opts = {},
   },
+  {
+    "adibhanna/nvim-newfile.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("nvim-newfile").setup {
+        -- Optional configuration
+      }
+    end,
+  },
 }
