b509ed934aff8f6e0f86e7b074cdd367e851d9eb
Erşan Işık <ersanisik@teknasyon.com>
2025-11-22 00:17:09 +0300

    feat(nvim) mini-files plugin

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -1,14 +1,12 @@
+local prefix = "<Leader>A"
 return {
   "yetone/avante.nvim",
-  event = "VeryLazy",
-  version = false, -- Never set this value to "*"! Never!
   opts = {
-    provider = "copilot", -- Recommend using Claude
-    auto_suggestions_provider = "copilot",
-    providers = {
-      copilot = {
-        model = "claude-haiku-4.5",
+    mappings = {
+      toggle = {
+        selection = prefix .. "C",
       },
+      zen_mode = prefix .. "z",
     },
   },
 }
